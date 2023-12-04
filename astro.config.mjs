04db0c0d5e85792174f8d4b224fa3c738f65f6d4
Tue Travel <tuh@smykkeria.dk>
2023-12-04 13:23:55 +0000

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://stalwart-marigold-78bfde.netlify.app"
+  site: "https://stalwart-marigold-78bfde.netlify.app",
+  integrations: [preact()]
 });
